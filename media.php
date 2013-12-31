178a9ba76e45bd6e98defbc780d1597ecf2ef44e
Omni Adams <omni@digitaldarkness.com>
2013-12-31 09:19:02 -0600

    Abstracted out pi username and address

--- media.php
+++ media.php
@@ -3,13 +3,15 @@
  * Frontend script for controlling a media player.
  */
 
+$pi = 'pi@ip-address';
 
 /**
  * Get the volume from a remote player.
- * @return integer Percent volume the remote server is playing at.
+ * @param string $pi User and host to log in to
+ * @return integer Percent volume the remote server is playing at
  */
-function getVolume() {
-    exec('ssh pi@192.168.1.87 amixer', $output);
+function getVolume($pi) {
+    exec('ssh ' . $pi . ' amixer', $output);
     $volume = array_pop($output);
     preg_match('/\[(\d*)/', $volume, $matches);
     return array_pop($matches);
@@ -19,25 +21,28 @@ function getVolume() {
 /**
  * Set the volume on the remote player.
  * @param integer $volume Percentage volume to play at.
+ * @param string $pi User and host to log in to
  */
-function setVolume($volume) {
+function setVolume($volume, $pi) {
     $volume = escapeshellarg($volume) . '%';
-    exec('ssh pi@192.168.1.87 amixer set PCM ' . $volume);
+    exec('ssh ' . $pi . ' amixer set PCM ' . $volume);
 }
 
 
 /**
  * Go to the next song and update the now playing file.
+ * @param string $pi User and host to log in to
  */
-function nextSong() {
-    exec('ssh pi@192.168.1.87 \'echo "pausing_keep_force pt_step 1" >> mplayer-control\'');
+function nextSong($pi) {
+    exec('ssh ' . $pi . ' \'echo "pausing_keep_force pt_step 1" >> mplayer-control\'');
     sleep(1);
-    exec('ssh pi@192.168.1.87 ./now-playing.sh');
+    exec('ssh ' . $pi . ' ./now-playing.sh');
 }
 
 
 /**
  * Return information about the currently playing song.
+ * @param string $pi User and host to log in to
  * @return array ['song', 'artist']
  */
 function getCurrentSong() {
@@ -53,41 +58,42 @@ function getCurrentSong() {
 /**
  * Toggle muting on or off.
  * @param boolean $mute Whether to mute or not.
+ * @param string $pi User and host to log in to
  */
-function mute($mute) {
+function mute($mute, $pi) {
     $mute = (int)$mute;
-    exec('ssh pi@192.168.1.87 echo "mute ' . $mute . '" >> mplayer-control');
+    exec('ssh ' . $pi . ' echo "mute ' . $mute . '" >> mplayer-control');
 }
 
 
-$volume = getVolume();
+$volume = getVolume($pi);
 if (isset($_POST['volume'])) {
     $volume = (int)$_POST['volume'];
-    setVolume($volume);
+    setVolume($volume, $pi);
 } elseif (isset($_POST['action'])) {
     switch ($_POST['action']) {
         case 'volumeUp':
             $volume += 5;
-            setVolume((int)$volume);
+            setVolume((int)$volume, $pi);
             break;
         case 'volumeDown':
             $volume -= 5;
-            setVolume((int)$volume);
+            setVolume((int)$volume, $pi);
             break;
         case 'next':
-            nextSong();
+            nextSong($pi);
             break;
         case 'mute':
-            mute(true);
+            mute(true, $pi);
             break;
         case 'unmute':
-            mute(false);
-            setVolume(60);
+            mute(false, $pi);
+            setVolume(60, $pi);
             break;
     }
 }
 
-$nowPlaying = getCurrentSong();
+$nowPlaying = getCurrentSong($pi);
 $nowPlaying['song'] = preg_replace('/^\d+ /', '', $nowPlaying['song']);
 ?>
 <!doctype html>
